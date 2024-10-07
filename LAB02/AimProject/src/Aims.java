fab59a1e56cc6b95e455f7ea24f1787ce2683f4b
Vũ Quang Thắng <thangbienhoa3gmail.com@MacBook-Pro-cua-Vu.local>
2024-10-07 16:09:11 +0700

    first commit

---
+++ LAB02/AimProject/src/Aims.java
@@ -0,0 +1,7 @@
+public class Aims {
+
+    public static void main(String[] args) {
+        DigitalVideoDisc obj1 = new DigitalVideoDisc("Random CD");
+        System.out.println(obj1.getTitle());
+    }
+}
---
+++ LAB02/AimProject/src/DigitalVideoDisc.java
@@ -0,0 +1,27 @@
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+    public DigitalVideoDisc(String title){
+        super();
+        this.title = title;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getCategory(){
+        return category;
+    }
+    public String getDirector(){
+        return director;
+    }
+    public int getlength(){
+        return length;
+    }
+    public float getCost(){
+        return cost;
+    }
+
+}
